5bd6312fe6dced802b50bbf1357fe9dacd06423d
languint <longuin@proton.me>
2025-03-16 17:32:46 -0500

    Add type checker.

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@
 
 void printHeader() {
     util::ansiColors("Compufactorio!", {AnsiColor::BrightGreen});
-    util::ansiColors("Compiled: " + static_cast<std::string>(__DATE__) + " " + __TIME__, {AnsiColor::BrightBlue});
     std::cout << "C++ Version: " << __cplusplus << "\n";
     std::cout << "---" << "\n";
     std::cout << "\n";
