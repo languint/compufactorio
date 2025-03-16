e294ab041a70958d89152ee0d260c16fa299b0f2
languint <longuint@proton.me>
2025-03-16 10:48:51 -0500

    Add printHeader.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
-void printHeader() {
+#include "util.h"
 
+void printHeader() {
+    util::ansiColors("Compufactorio!", {AnsiColor::BrightGreen});
+    util::ansiColors("Compiled: " + static_cast<std::string>(__DATE__) + " " + __TIME__, {AnsiColor::BrightBlue});
+    std::cout << "C++ Version: " << __cplusplus << "\n";
+    std::cout << "---" << "\n";
+    std::cout << "\n";
 }
 
 int main() {
-
+    printHeader();
     return 0;
 }
 
